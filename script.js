a4729b6e7eed739217ff9bc564012aa24e0cd5ea
Alp Bulut <83169689+AlbusNimbus@users.noreply.github.com>
2022-07-06 22:51:09 +0200

    feat: Display Controller

--- script.js
+++ script.js
@@ -1,28 +1,64 @@
 
 
 
+const Player = () => {
+    let sign = "cross"
+    return 
+};
+let player = "cross"
+const gameController = (() => {
+    let board = Array(9)
+    let nextPlayer = "cross"
 
-const Gameboard = () => {
-    const putSign = (sign, cellId) => {
-    const field = document.querySelector(`.cell-(${cellId})`);
-    field.classList.add('put' + sign)
-    console.log("hello")
+    const playRound = (player, field) => {
+        if (!getFieldValue(field) && myTurn(player)) {
+            board[field] = player
+            displayController.updateField(player, field)
+            playerTurn(player)
+        }
     }
-    return { putSign }
-}
+
+    const playerTurn = (player) => {
+        if(player.sign === "cross"){
+            nextPlayer = "circle"
+        } else{
+            nextPlayer = "cross"
+        }
+    }
+    const myTurn = (player) => {
+        if(nextPlayer === "cross"){
+            return true
+        }
+        return false
+    }
+
+    const getFieldValue = (field) => {
+        return board[field]
+    }
+    return {playRound, getFieldValue}
+})();
 
 
-const gameController = () => {
+const displayController = (() => {
+    const fields = document.querySelectorAll('.field')
 
-}
+    fields.forEach((field) => {
+        field.addEventListener("click", (e) => {
+            gameController.playRound(player,field.id)
+        })
+    })
 
-const displayController = () => {
-    const cells = document.querySelectorAll('.cell')
-    cells.forEach(cell => {
-        cell.addEventListener('click', console.log(cell.id))
-    });
+    const updateField = (player, field) => {
+            if (player.sign === "circle") {
+                fields[field].querySelector(".circle").classList.add('active')
+            } else if (player === "cross"){
+                fields[field].querySelector(".cross").classList.add('active')
+            }  
+    }
+    return {updateField}
+})();
 
-    return { cells}
-}
 
-const dp = displayController()
+const AI = (() => {
+
+})();
